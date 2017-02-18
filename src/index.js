a23df04ae72fbcaf0adb17d554cc655dea1e37fc
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-02-18 10:22:03 -0500

    copied module creating code

--- src/index.js
+++ src/index.js
@@ -1,1 +1,90 @@
 'use strict'
+
+const rootFolder = process.env.HOME
+const path = require('path')
+const fs = require('fs')
+const R = require('ramda')
+const semver = require('semver')
+const execa = require('execa')
+const mkdirp = require('mkdirp')
+
+function getVersion (folder) {
+  const packageFilename = path.join(folder, 'package.json')
+  return new Promise((resolve, reject) => {
+    try {
+      fs.readFile(packageFilename, 'utf8', (err, s) => {
+        if (err) {
+          return reject(err)
+        }
+        const json = JSON.parse(s)
+        resolve({
+          folder: folder,
+          filename: packageFilename,
+          name: json.name,
+          version: json.version,
+          main: path.join(folder, json.main)
+        })
+      })
+    } catch (err) {
+      reject()
+    }
+  })
+}
+
+function getVersionSafe (folder) {
+  return getVersion(folder).catch(() => undefined)
+}
+
+function byVersion (a, b) {
+  return semver.compare(a.version, b.version)
+}
+
+function folderSearch (name) {
+  const args = [rootFolder, '-maxdepth', '4', '-type', 'd', '-name', name]
+  return execa('find', args).then(result => result.stdout.split('\n'))
+}
+function findModule (name) {
+  const fullLine = `node_modules/${name}`
+  return folderSearch(name)
+    .then(lines => lines.filter(line => line.includes(fullLine)))
+    .then(folders => Promise.all(folders.map(getVersionSafe)))
+    .then(R.filter(R.is(Object)))
+    .then(R.sort(byVersion))
+}
+
+function print (modules) {
+  const different = R.uniqBy(R.prop('version'))(modules)
+  console.log('%d different version', different.length)
+  console.log(different)
+}
+
+function installMain (p) {
+  if (!p) {
+    console.error('nothing to install')
+    // TODO: use real NPM install in this case
+    return
+  }
+  const destination = path.join(process.cwd(), 'node_modules', p.name)
+  console.log('installing', p)
+  console.log('as', destination)
+
+  mkdirp.sync(destination)
+  const pkg = {
+    name: p.name,
+    main: p.main,
+    version: p.version,
+    description: 'fake module created by "have-it" pointing at existing module'
+  }
+  const filename = path.join(destination, 'package.json')
+  const json = JSON.stringify(pkg, null, 2)
+  fs.writeFileSync(filename, json, 'utf8')
+}
+
+findModule('debug')
+  .then(R.tap(print))
+  .then(R.last)
+  .then(installMain)
+
+// const existingPath = '/Users/gleb/git/training/node/foo'
+// const newPath = '/Users/gleb/git/training/node/have-it/node_modules/foo'
+// fs.linkSync(existingPath, newPath)
