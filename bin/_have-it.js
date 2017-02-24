8a4221ba19bb94ab3b688fbfc9a0baa79226e88b
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-02-24 15:57:33 -0500

    feat(bin): allow dev alias _have but with error message

--- bin/_have-it.js
+++ bin/_have-it.js
@@ -1,5 +1,12 @@
 #!/usr/bin/env node
 
 // useful for local testing without building
-
-require('./have-it.js')
+try {
+  require('./have-it.js')
+} catch (err) {
+  console.error(err)
+  console.error('Could not load ./have-it.js')
+  console.error('_have alias is only meant for local development!')
+  console.error('Use "have" or "have-it" please')
+  process.exit(1)
+}
